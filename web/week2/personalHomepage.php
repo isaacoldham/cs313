2050e62b9d9af389e10867c9fbe4c4ee3d1ecd88
Isaac Oldham <isaac@robertoldham.com>
2021-01-14 15:36:56 -0700

    add date

--- web/week2/personalHomepage.php
+++ web/week2/personalHomepage.php
@@ -8,28 +8,29 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" description="About me">
   <title>About Me</title>
   <script>
-      function showImage() {
-          document.getElementById("video").style.display = "block";
-      }
+    function showImage() {
+      document.getElementById("video").style.display = "block";
+    }
   </script>
 
 </head>
 
 <body>
   <header>
-  <h1>Welcome to Isaac Oldham's Home Page!</h1>
-  <div id="menu">
-    <a href="../home.php">CSE 341 Assignments</a>
-  </div>
+    <h1>Welcome to Isaac Oldham's Home Page!</h1>
+    <div id="menu">
+      <a href="../home.php">CSE 341 Assignments</a>
+    </div>
+  </header>
   <p>Hey everyone! My name is <strong>Isaac Oldham!</strong> My major is software engineering and I'm from Spanish Fork Utah.</p>
   <div class="picDiv">Below are some recent pictures from my life.</div>
   <div style="text-align:center" id="clickable">
-    <img src="IMG_20201007_224253.jpg"class="clickableImage"/>
-    <img src="IMG_20201126_093233.jpg"class="clickableImage"/>
-    <img src="IMG_20201120_134242.jpg"class="clickableImage"/>
+    <img src="IMG_20201007_224253.jpg" class="clickableImage" />
+    <img src="IMG_20201126_093233.jpg" class="clickableImage" />
+    <img src="IMG_20201120_134242.jpg" class="clickableImage" />
   </div>
   <div class="picDiv">
-    <button onclick="showImage()">Click here</button> 
+    <button onclick="showImage()">Click here</button>
     To see a super cool video of the world record ski jump!
   </div>
   <iframe id="video" src="https://www.youtube.com/embed/-RYkapHBVs8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -37,9 +38,8 @@
     <hr>
     Education is the difference between wishing you could help others and being able to help them. <b>- President Nelson</b>
   </footer>
-    
-  </header>
+  <?php echo date("D M d, Y G:i a"); ?>
+
 </body>
 
 </html>
-
