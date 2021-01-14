19a3924a3d22071c701eefc1c5d98b100d8a7712
Isaac Oldham <isaac@robertoldham.com>
2021-01-13 17:24:41 -0700

    Adds new home.php and general.css files.

---
+++ web/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--https://github.com/isaacoldham/cs313
+    Web page to the repo for all of the assignments for cs313-->
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="general.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" description="Homepage for all assignments">
+  <title>Home</title>
+</head>
+
+<body style="background-color:white; color:black; font-size:larger;">
+  <header>
+    <h1 style="text-align:center; margin-top: 100px; font-weight: bold;">Here are all the assignments for the semester!</h1>
+  </header>
+</body>
+
+</html>
