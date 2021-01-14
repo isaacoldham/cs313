c36ae52c37e1cdd3c2ebb2fd2ca638c72e1ebfb5
Isaac Oldham <isaac@robertoldham.com>
2021-01-13 18:44:41 -0700

    Week 2 files

---
+++ web/week2/personalHomepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--https://github.com/isaacoldham/cs313-->
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" description="About me">
+  <title>About Me</title>
+</head>
+
+<body>
+  <header>
+  <h1>Welcome to Isaac Oldham's Home Page!</h1>
+  <hr/>
+  <div>
+    <a href="../home.php">CS-213 Assignments</a>
+  </div>
+  <hr/>
+  <p style="text-align:center; font-family:arial;">Hey everyone! My name is <strong>Isaac Oldham!</strong> I'm majoring in software engineering and I'm from Spanish Fork Utah.</p>
+  <div>Below are some recent pictures from my life. Click here to view them.</div>
+  <div style="text-align:center">
+    <img src="../me.jpg" alt="A pic of Me!" style="height:320px; width:auto;"/>
+  </div>
+  <blockquote style="text-align:center; font-family:courier;">
+    Education is the difference between wishing you could help others and being able to help them. <b>- President Nelson</b>
+  </blockquote>
+    
+  </header>
+</body>
+
+</html>
