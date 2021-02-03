f3d53ecd9e42c75f4ecb11d146960e59db370850
Isaac Oldham <isaac@robertoldham.com>
2021-02-02 19:03:06 -0700

    function

--- web/scriptures.php
+++ web/scriptures.php
@@ -13,9 +13,20 @@ try {
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    function getScriptures ($book){
+        $statement = $db->prepare('SELECT * FROM scriptures AS s WHERE s.book =:book');
+        $statement->bindValue(':book', $book, PDO::PARAM_STR);
+        $statement->execute();
+        $scriptures = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $statement->closeCursor();
+        return $scriptures;
+    }
+
 } catch (PDOException $ex) {
     echo 'Error!: ' . $ex->getMessage();
     die();
+
 }
 ?>
 <!DOCTYPE html>
@@ -38,9 +49,31 @@ try {
 ?>
 </div>
 
-<form>
-
+<form action="scriptures.php">
+    <div>Please enter a book:</div>
+    <input type="text" name="book">
+    <input type="submit" value="Submit">
+    <input type="hidden" name="action" value="searchScripture">
 </form>
+
+<?php
+$action = filter_input(INPUT_POST, 'action');
+
+switch ($action){
+    case 'searchScripture': 
+        $book = filter_input(INPUT_POST, 'book', FILTER_SANITIZE_STRING);
+        $scriptures = getScriptures($book);
+        echo $scriptures;
+    break;
+
+    
+}
+
+
+
+?>
+
+
 </body>
 
 
