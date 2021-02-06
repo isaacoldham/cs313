82226d18f0fe73ead2f0965437f59df9f7461462
Isaac Oldham <isaac@robertoldham.com>
2021-02-06 16:21:24 -0700

    changed

--- web/week05/allSkis.php
+++ web/week05/allSkis.php
@@ -74,7 +74,7 @@ try {
   </div>
 
   <form action="searchSkis.php" method="post">
-    <div>Have something particular in mind? Search by length here!<br>
+    <div style="font-size=1.5erm;">Have something particular in mind? Search by length here!<br>
       <input type="number" name="searchSize" style="text-align:right">
       <input type="submit" name="submit" value="Search">
     </div>
@@ -86,23 +86,23 @@ try {
 
   <?php
 
-  if (isset($_POST['ski1'])) {
-    $_SESSION["ski1"] = $_POST['ski1'];
-  } elseif (isset($_POST['ski2'])) {
-    $_SESSION["ski2"] = $_POST['ski2'];
-  } elseif (isset($_POST['ski3'])) {
-    $_SESSION["ski3"] = $_POST['ski3'];
-  } elseif (isset($_POST['ski4'])) {
-    $_SESSION["ski4"] = $_POST['ski4'];
-  } elseif (isset($_POST['ski5'])) {
-    $_SESSION["ski5"] = $_POST['ski5'];
-  } elseif (isset($_POST['ski6'])) {
-    $_SESSION["ski6"] = $_POST['ski6'];
-  } elseif (isset($_POST['ski7'])) {
-    $_SESSION["ski7"] = $_POST['ski7'];
-  } elseif (isset($_POST['ski8'])) {
-    $_SESSION["ski8"] = $_POST['ski8'];
-  }
+  // if (isset($_POST['ski1'])) {
+  //   $_SESSION["ski1"] = $_POST['ski1'];
+  // } elseif (isset($_POST['ski2'])) {
+  //   $_SESSION["ski2"] = $_POST['ski2'];
+  // } elseif (isset($_POST['ski3'])) {
+  //   $_SESSION["ski3"] = $_POST['ski3'];
+  // } elseif (isset($_POST['ski4'])) {
+  //   $_SESSION["ski4"] = $_POST['ski4'];
+  // } elseif (isset($_POST['ski5'])) {
+  //   $_SESSION["ski5"] = $_POST['ski5'];
+  // } elseif (isset($_POST['ski6'])) {
+  //   $_SESSION["ski6"] = $_POST['ski6'];
+  // } elseif (isset($_POST['ski7'])) {
+  //   $_SESSION["ski7"] = $_POST['ski7'];
+  // } elseif (isset($_POST['ski8'])) {
+  //   $_SESSION["ski8"] = $_POST['ski8'];
+  // }
 
   //echo variable info
   // echo "<div>post</div>";
--- web/week05/mensSkis.php
+++ web/week05/mensSkis.php
@@ -52,29 +52,29 @@ try {
             <a class="menuItem" href="womensSkis.php">Womens skis</a>
         </div>
     </header>
-    
+
     <h2>Mens Skis</h2>
 
-    <form onsubmit="CheckNotNull()" action="" method="post" id="formId">
-        <div style="text-align:center" id="wrapper">
 
-            <?php
-            $type = 'mens';
-            $stmt = $db->prepare('SELECT s.length, s.ski_name, s.make, s.img_url, s.type FROM skis AS s WHERE s.type=:type');
-            $stmt->bindValue(':type', $type, PDO::PARAM_STR);
-            $stmt->execute();
-            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    <div style="text-align:center" id="wrapper">
 
-            foreach ($rows as $row) {
-                echo '<div class="item">' . $row['ski_name'];
-                echo ' - <span style="font-weight: none;">' . $row['make'];
-                echo ' ' . $row['length'] . 'cm';
-                echo '<img src="' . $row['img_url'] . '" class="clickableImage" />';
-                echo '</span></div><br>';
-            };
-            ?>
+        <?php
+        $type = 'mens';
+        $stmt = $db->prepare('SELECT s.length, s.ski_name, s.make, s.img_url, s.type FROM skis AS s WHERE s.type=:type');
+        $stmt->bindValue(':type', $type, PDO::PARAM_STR);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            <!-- <div class="item" id="ski5">
+        foreach ($rows as $row) {
+            echo '<div class="item">' . $row['ski_name'];
+            echo ' - <span style="font-weight: none;">' . $row['make'];
+            echo ' ' . $row['length'] . 'cm';
+            echo '<img src="' . $row['img_url'] . '" class="clickableImage" />';
+            echo '</span></div><br>';
+        };
+        ?>
+
+        <!-- <div class="item" id="ski5">
         <img src="ski5.jpg" class="clickableImage" />
         <label for="ski5">Atomic - Vantage 75</label>
         <button type="submit" name="ski5" style="text-align:center;" value="true">Add to Cart</button>
@@ -94,8 +94,13 @@ try {
         <label for="ski8">Volkl - M5 Mantra</label>
         <button type="submit" name="ski8" style="text-align:center;" value="true">Add to Cart</button>
       </div> -->
-        </div>
+    </div>
 
+    <form action="searchSkis.php" method="post">
+        <div style="font-size=1.5erm;">Have something particular in mind? Search by length here!<br>
+            <input type="number" name="searchSize" style="text-align:right">
+            <input type="submit" name="submit" value="Search">
+        </div>
     </form>
 
     <!-- <div style="width: 100%; float: clear; box-sizing: border-box; clear: both;">
