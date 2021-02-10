01b29d05c4b3eb91936e64145bbe8b629868d8f6
Isaac Oldham <isaac@robertoldham.com>
2021-02-09 18:08:02 -0700

    some changes

--- web/scriptures.php
+++ web/scriptures.php
@@ -14,7 +14,8 @@ try {
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    function getScriptures ($book){
+    function getScriptures($book)
+    {
         $statement = $db->prepare('SELECT * FROM scriptures AS s WHERE s.book =:book');
         $statement->bindValue(':book', $book, PDO::PARAM_STR);
         $statement->execute();
@@ -22,53 +23,66 @@ try {
         $statement->closeCursor();
         return $scriptures;
     }
-
 } catch (PDOException $ex) {
     echo 'Error!: ' . $ex->getMessage();
     die();
-
 }
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 
 </head>
+
 <body>
 
-<div>
-<?php
-    foreach ($db->query('SELECT * FROM scriptures') as $row)
-    {
-      echo '<div><strong>' . $row['book'];
-      echo ' ' . $row['chapter'];
-      echo ':' . $row['verse'];
-      echo '</strong> - ' . $row['content'];
-      echo '</div><br>';
-    };
-?>
-</div>
+    <div>
+        <?php
+        foreach ($db->query('SELECT * FROM scriptures') as $row) {
+            echo '<div><strong>' . $row['book'];
+            echo ' ' . $row['chapter'];
+            echo ':' . $row['verse'];
+            echo '</strong> - ' . $row['content'];
+            echo '</div><br>';
+        };
+        ?>
+    </div>
 
-<form action="scriptures.php">
-    <div>Please enter a book:</div>
-    <input type="text" name="book">
-    <input type="submit" value="Submit">
-    <input type="hidden" name="action" value="searchScripture">
-</form>
+    <form action="scriptures.php">
+        <div>Please enter a book:</div>
+        <input type="text" name="book">
+        <div>Please enter a chapter:</div>
+        <input type="number" name="chapter">
+        <div>Please enter a verse:</div>
+        <input type="number" name="verse">
+        <div>Please enter the scripture:</div>
+        <textarea name="scripture"></textarea>
+        <?php
+        foreach ($db->query('SELECT name FROM topic') as $row) {
+            echo '<div><input type="checkbox" name="topic[]" value="'.$row['name'].'">' . $row['name'];
+        };
+        ?>
+        <input type="submit" value="Submit">
+        <input type="hidden" name="action" value="searchScripture">
+    </form>
 
-<?php
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-}
-switch ($action){
-    case 'searchScripture': 
-        $book = filter_input(INPUT_POST, 'book', FILTER_SANITIZE_STRING);
-        $scriptures = getScriptures($book);
-        echo $scriptures;
-    break;
-    default:break;
-}
-?>
+
+    <?php
+    $action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+    }
+    switch ($action) {
+        case 'searchScripture':
+            $book = filter_input(INPUT_POST, 'book', FILTER_SANITIZE_STRING);
+            $scriptures = getScriptures($book);
+            echo $scriptures;
+            break;
+        default:
+            break;
+    }
+    ?>
 </body>
+
 </html>
