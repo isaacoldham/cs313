f587ba9e318fd734243bdc8b386e6f39f9b50c25
Isaac Oldham <isaac@robertoldham.com>
2021-02-12 19:49:15 -0700

    more testing

--- web/week05/confirmLogin.php
+++ web/week05/confirmLogin.php
@@ -8,30 +8,22 @@ try {
     $dbUser = $dbOpts["user"];
     $dbPassword = $dbOpts["pass"];
     $dbName = ltrim($dbOpts["path"], '/');
-  
+
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-  
+
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-    //   function getSkis ($skis){
-    //       $statement = $db->prepare('SELECT * FROM skriptures AS s WHERE s.book =:book');
-    //       $statement->bindValue(':book', $skis, PDO::PARAM_STR);
-    //       $statement->execute();
-    //       $scriptures = $statement->fetchAll(PDO::FETCH_ASSOC);
-    //       $statement->closeCursor();
-    //       return $scriptures;
-    //   }
-  } catch (PDOException $ex) {
+
+} catch (PDOException $ex) {
     echo 'Error!: ' . $ex->getMessage();
     die();
-  }
+}
 
-    $stmt = $db->query('SELECT username, password FROM user_login WHERE username =:username AND password =:password');
-    $stmt->bindValue(':username', $_POST["username"], PDO::PARAM_STR);
-    $stmt->bindValue(':password', $_POST["password"], PDO::PARAM_STR);
-    $stmt->execute();
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+// $stmt = $db->query('SELECT username, password FROM user_login WHERE username =:username AND password =:password');
+// $stmt->bindValue(':username', $_POST["username"], PDO::PARAM_STR);
+// $stmt->bindValue(':password', $_POST["password"], PDO::PARAM_STR);
+// $stmt->execute();
+// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 // if($rows != NULL){
 //     header("Location: https://floating-skis.herokuapp.com/week05/editSkis.php");
@@ -44,7 +36,9 @@ try {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <body>
-<div>Please wait while you are redirected.</div>
+    <div>Please wait while you are redirected.</div>
 </body>
+
 </html>
