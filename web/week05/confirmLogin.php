dc0260a52be38eaa375820fb1a8e4033c9f20539
Isaac Oldham <isaac@robertoldham.com>
2021-02-18 22:05:28 -0700

    update password to hash

--- web/week05/confirmLogin.php
+++ web/week05/confirmLogin.php
@@ -20,25 +20,33 @@ try {
 
 $username = htmlspecialchars($_POST["username"]);
 $password = htmlspecialchars($_POST["password"]);
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-
-$stmt = $db->prepare('SELECT first_name, password FROM user_rental WHERE username =:username AND password =:password;');
-$stmt->bindValue(':username', $username, PDO::PARAM_STR);
-$stmt->bindValue(':password', $password, PDO::PARAM_STR);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$_SESSION["rows"] = $rows;
-
-//error_log(print_r($rows, true));
-
-if($rows != NULL){
-    header("Location: https://floating-skis.herokuapp.com/week05/editSkis.php");
-    $_SESSION["login"] = true;
+if (password_verify($password, $hashedPassword)) {
+    $queryStmt = 'UPDATE user_rental SET password='.$hashedPassword.' WHERE username ='.$username.';';
+    $stmt2 = $db->prepare('UPDATE');
+    $stmt2->execute();
+    $rows2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-else{
-    header("Location: https://floating-skis.herokuapp.com/week05/login.php");
-    $_SESSION["login"] = false;
-}
+
+// $stmt = $db->prepare('SELECT first_name, password FROM user_rental WHERE username =:username AND password =:password;');
+// $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+// $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+// $stmt->execute();
+// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $_SESSION["rows"] = $rows;
+
+// //error_log(print_r($rows, true));
+
+// if ($rows != NULL) {
+//     $_SESSION["login"] = true;
+//     header("Location: https://floating-skis.herokuapp.com/week05/editSkis.php");
+//     die();
+// } else {
+//     $_SESSION["login"] = false;
+//     header("Location: https://floating-skis.herokuapp.com/week05/login.php");
+//     die();
+// }
 ?>
 
 
