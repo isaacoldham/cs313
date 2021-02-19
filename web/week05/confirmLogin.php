8e397cb5755153352c1ca02ad4f43576d99e0d45
Isaac Oldham <isaac@robertoldham.com>
2021-02-19 15:16:31 -0700

    add bind variables to fix query statement

--- web/week05/confirmLogin.php
+++ web/week05/confirmLogin.php
@@ -29,10 +29,11 @@ $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 if (password_verify($password, $hashedPassword)) {
     var_dump($hashedPassword);
-    $queryStmt = 'UPDATE user_rental SET password='.$hashedPassword.' WHERE username ='.$username.';';
-    $stmt2 = $db->prepare($queryStmt);
-    $stmt2->execute();
-    $rows2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //$queryStmt = 'UPDATE user_rental SET password='.$hashedPassword.' WHERE username ='.$username.';';
+    $stmt2 = $db->prepare('UPDATE user_rental SET password=:hashedPassword WHERE username =:username;');
+    $stmt->bindValue(':hashedPassword', $hashedPassword, PDO::PARAM_STR);
+//    $stmt2->execute();
+//    $rows2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 // $stmt = $db->prepare('SELECT first_name, password FROM user_rental WHERE username =:username AND password =:password;');
