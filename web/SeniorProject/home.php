395bfd8c1f33c21a6deae8443f70aa093f2c3ad5
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-11-04 21:03:13 -0600

    home.php and sign_up.php

---
+++ web/SeniorProject/home.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+
+//Connect to the database
+try {
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"], '/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //   function getSkis ($skis){
+    //       $statement = $db->prepare('SELECT * FROM skriptures AS s WHERE s.book =:book');
+    //       $statement->bindValue(':book', $skis, PDO::PARAM_STR);
+    //       $statement->execute();
+    //       $scriptures = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //       $statement->closeCursor();
+    //       return $scriptures;
+    //   }
+} catch (PDOException $ex) {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+//If the $_SESSION["login"] is not equal to true then send the user back to the login page.
+if ($_SESSION["login"] != true) {
+    header("Location: https://floating-skis.herokuapp.com/SeniorProject/login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!--https://github.com/isaacoldham/cs313-->
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" description="Ski Shop">
+    <title>The RD Converter</title>
+    <script>
+    </script>
+
+</head>
+
+<body>
+    <header>
+        <h1>The RD Converter</h1>
+        <div id="menu">
+            
+        </div>
+
+    </header>
+
+
+    <div style="text-align:center">
+        <form method="post" action="edit.php">
+            <?php
+            foreach ($db->query('SELECT length, ski_name, make, img_url, ski_id FROM skis ORDER BY ski_id;') as $row) {
+                echo '<div>' . $row['ski_name'];
+                echo ' - <span style="font-weight: none;">' . $row['make'];
+                echo ' ' . $row['length'] . 'cm ';
+                echo '</span> <button type="submit" name="ski_id" value="'.$row['ski_id'].'">Edit</button>';
+                echo '<button type="submit" formaction="delete.php" name="ski_id" value="'.$row['ski_id'].'">Delete</button>';
+                echo '</div><br><br>';
+            };
+            ?>
+        </form>
+    </div>
+
+    <div style="width: 100%; float: clear; box-sizing: border-box; clear: both;">
+        <br>
+    </div>
+
+    <br><br><br><br>
+    <!-- <div class="picDiv">
+    <button onclick="showImage()">Click here</button>
+    To see a super cool video of the world record ski jump!
+  </div> -->
+
+    <footer style="padding:10px; margin-top:20px;">
+        <hr>
+        To log out <a href="logOut.php">click here</a>.
+    </footer>
+
+</body>
+
+</html>
---
+++ web/SeniorProject/sign_up.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!--https://github.com/isaacoldham/cs313-->
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" description="Ski Shop">
+    <title>The RD Converter</title>
+    <script>
+    </script>
+
+</head>
+
+<body>
+    <header>
+        <h1>The RD Converter</h1>
+        <div id="menu"> </div>
+
+    </header>
+
+    <h2>Please Login</h2>
+
+    <?php
+    if ($_SESSION["badLogin"] = true) {
+        echo '<div style="color:red;">There was an error with your login. Please try again.</div>';
+    }
+    ?>
+
+    <form method="post" action="addLogin.php" class="login">
+        <div style="margin:10px;">
+            Please enter a username:
+            <input type="text" name="username">
+        </div>
+        <div style="margin-bottom:10px;">
+            Please enter a password:
+            <input type="password" name="password">
+        </div>
+        <div style="margin-bottom:10px;">
+            Please re-enter your password:
+            <input type="password" name="password2">
+        </div>
+
+        <input type="submit" value="Login">
+    </form>
+
+    <br><br>
+    
+
+
+    <br><br><br><br>
+    <!-- <div class="picDiv">
+    <button onclick="showImage()">Click here</button>
+    To see a super cool video of the world record ski jump!
+  </div> -->
+
+</body>
+
+</html>
