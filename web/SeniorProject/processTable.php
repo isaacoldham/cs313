3496501e9786a385591bdee70c1ab203d7c50a69
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-12-07 21:49:25 -0700

    yeah this probs doesn't work

--- web/SeniorProject/processTable.php
+++ web/SeniorProject/processTable.php
@@ -156,7 +156,7 @@ else {
 </html>
 <script>
     let jsonString = document.getElementById('jsonDiv').innerHTML;
-    jsonString.replace("Array ( [0] => Array ( [json] => ", "");
+    jsonString.replace("Array (     [0] =&gt; Array         (             [json] =&gt; ", "");
     console.log(jsonString);
     let jsonPretty = JSON.stringify(JSON.parse(jsonString),null,2);  
     document.getElementById('jsonDiv').innerHTML = jsonPretty;
