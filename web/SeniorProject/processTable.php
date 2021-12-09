9012f642368c6699357bbe88914039ae95b6ef06
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-12-08 18:10:22 -0700

    now try this

--- web/SeniorProject/processTable.php
+++ web/SeniorProject/processTable.php
@@ -159,7 +159,7 @@ else {
     var jsonString = document.getElementById('jsonDiv').innerText;
     jsonString = jsonString.substring(18, jsonString.length - 1);
     console.log(jsonString);
-    var jsonPretty = JSON.stringify(JSON.parse(jsonString));  
+    var jsonPretty = JSON.stringify(jsonString);  
     document.getElementById('jsonDiv').innerText = jsonPretty;
     console.log(jsonPretty);
 </script>
