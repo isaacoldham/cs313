0b6d9218a3a75bd338893a24b3e9797ad95916a4
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-12-08 19:29:39 -0700

    Please work!

--- web/SeniorProject/processTable.php
+++ web/SeniorProject/processTable.php
@@ -159,6 +159,7 @@ else {
     var jsonString = document.getElementById('jsonDiv').innerText;
     jsonString = jsonString.substring(18, jsonString.length - 2)
     jsonString = jsonString.substring((jsonString.length / 2) + 5)
+    jsonString = jsonString.toString();
     console.log('|'+jsonString+'|')
     jsonString = JSON.parse(jsonString)
     console.log('|'+jsonString+'|')
